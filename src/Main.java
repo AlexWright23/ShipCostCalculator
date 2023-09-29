34d05c9502eb14a430d944617f1637c33d98059b
AlexWright23 <honeybadger937@gmail.com>
2023-09-29 13:58:31 -0400

    Added pseudo code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
 
+//Class itemPrice
+//        main()
+//        num = 100
+//        output "Please enter price of the item $"
+//        input itemPrice
+//        if itemPrice >= 100 then
+//        shippingCost = 0
+//        else shippingCost = 0.02 * itemPrice
+//        totalPrice = itemPrice + shippingCost
+//        output "Your total cost is $ "
+//        return
+//        endClass
+
 public class Main {
     public static void main(String[] args)
     {
